ce241678a15abea507d13a66a064f1b7c4c8a276
sudeen <ranjitkarsudeen14@gmail.com>
2020-03-21 19:39:56 +1100

    creating the services block

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,6 +7,7 @@ import ButtonArrow from "../components/ui/ButtonArrow";
 import Typography from "@material-ui/core/Typography";
 
 import animationData from "../animations/landinganimation/data";
+import customSoftwareIcon from "../assets/Custom Software Icon.svg";
 
 const useStyles = makeStyles(theme => ({
   animation: {
@@ -33,16 +34,16 @@ const useStyles = makeStyles(theme => ({
     marginTop: "1em",
   },
   learnButtonHero: {
-    borderColor: theme.palette.common.blue,
-    color: theme.palette.common.blue,
-    borderWidth: 2,
-    textTransform: "none",
-    borderRadius: 50,
-    fontFamily: "Roboto",
-    fontWeight: "bold",
+    ...theme.typography.learnButton,
     fontSize: "0.9em",
     height: 45,
     width: 145,
+  },
+  learnButton: {
+    ...theme.typography.learnButton,
+    fontSize: "0.7rem",
+    height: 35,
+    padding: 5
   },
   mainContainer: {
     marginTop: "5em",
@@ -59,6 +60,13 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.down("xs")]: {
       marginLeft: 0,
     },
+  },
+  specialText: {
+    fontFamily: "Pacifico",
+    color: theme.palette.common.orange
+  },
+  subtitle: {
+    marginBottom: "1em"
   },
 }));
 
@@ -78,7 +86,7 @@ export default function LandingPage() {
 
   return (
     <Grid container direction="column" className={classes.mainContainer}>
-      <Grid item>
+      <Grid item> {/*-----Hero Block-----*/}
         <Grid container justify="flex-end" alignItems="center" direction="row">
           <Grid sm item className={classes.heroTextContainer}>
             <Typography variant="h2" align="center">
@@ -110,6 +118,29 @@ export default function LandingPage() {
           </Grid>
           <Grid sm item className={classes.animation}>
             <Lottie options={defaultOptions} height={"100%"} width={"100%"} />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item> {/*-----Services Block-----*/}
+        <Grid container direction="row">
+          <Grid item>
+            <Typography variant="h4">
+            Custom Software Development
+            </Typography>
+            <Typography variant="subtitle1" className={classes.subtitle}>
+              Save Energy. Save Time. Save Money.
+            </Typography>
+            <Typography variant="subtitle1">
+              Complete digital solution, from investigation to{" "}
+              <span className={classes.specialText}>celebration.</span>
+            </Typography>
+            <Button variant="outlined" className={classes.learnButton}>
+            <span style={{ marginRight: 10 }}>Learn More</span>
+            <ButtonArrow width={10} height={10} fill={theme.palette.common.blue}></ButtonArrow>
+            </Button>
+          </Grid>
+          <Grid item>
+            <img alt="custom software icon" src={customSoftwareIcon} />
           </Grid>
         </Grid>
       </Grid>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -6,6 +6,7 @@ import {
 
 const arcBlue = "#0B72B9";
 const arcOrange = "#FFBA60";
+const arcGrey = "#868686"
 
 export default createMuiTheme({
   palette: {
@@ -31,8 +32,33 @@ export default createMuiTheme({
       fontFamily: "Ralewway",
       fontWeight: 700,
       fontSize: "2.5rem",
-      color: `${arcBlue}`,
+      color: arcBlue,
       lineHeight: 1.5,
+    },
+    h3: {
+      fontFamily: "Pacifico",
+      fontSize: "2.5rem",
+      color: arcBlue
+    },
+    h4: {
+      fontFamily: "Raleway",
+      fontSize: "1.75rem",
+      color: arcBlue,
+      fontWeight: 700
+    },
+    subtitle1: {
+      fontSize: "1.25rem",
+      fontWeight: 300,
+      color: arcGrey
+    },
+    learnButton: {
+    borderColor: arcBlue,
+    color: arcBlue,
+    borderWidth: 2,
+    textTransform: "none",
+    borderRadius: 50,
+    fontFamily: "Roboto",
+    fontWeight: "bold",
     },
   },
 });
