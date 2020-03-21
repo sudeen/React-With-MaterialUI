f4de15087751d74f20680d219137e6f51c429fc0
sudeen <ranjitkarsudeen14@gmail.com>
2020-03-21 22:48:01 +1100

    created an informational block

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -14,6 +14,7 @@ import customSoftwareIcon from "../assets/Custom Software Icon.svg";
 import mobileAppIcon from "../assets/mobileIcon.svg";
 import websitesIcon from "../assets/websiteIcon.svg";
 import revolutionBackground from "../assets/repeatingBackground.svg";
+import infoBackground from "../assets/infoBackground.svg";
 
 const useStyles = makeStyles(theme => ({
   animation: {
@@ -111,12 +112,21 @@ const useStyles = makeStyles(theme => ({
       width: "100%",
     },
   },
+  infoBackground: {
+    backgroundImage: `url(${infoBackground})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100%",
+    width: "100%",
+  },
 }));
 
 export default function LandingPage() {
   const classes = useStyles();
   const theme = useTheme();
   const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+  const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
 
   //   Copied this from github docs
   const defaultOptions = {
@@ -328,6 +338,86 @@ export default function LandingPage() {
             </CardContent>
           </Card>
           <div className={classes.revolutionBackground} />
+        </Grid>
+      </Grid>
+      {/*-----Information Block-----*/}
+      <Grid item>
+        <Grid
+          container
+          style={{ height: "80em" }}
+          alignItems="center"
+          direction="row"
+        >
+          {/* Wrapping up both contact us and about us card */}
+          <Grid
+            item
+            container
+            style={{
+              position: "absolute",
+              textAlign: matchesXS ? "center" : "inherit",
+            }}
+            direction={matchesXS ? "column" : "row"}
+            spacing={matchesXS ? 10 : 0}
+          >
+            {/* Card of About Us */}
+            <Grid
+              item
+              sm
+              style={{ marginLeft: matchesXS ? 0 : matchesSM ? "2em" : "5em" }}
+            >
+              <Grid container direction="column">
+                <Typography variant="h2" style={{ color: "white" }}>
+                  About Us
+                </Typography>
+                <Typography variant="subtitle2">Let's get personal.</Typography>
+                <Grid item>
+                  <Button
+                    variant="outlined"
+                    style={{ color: "white", borderColor: "white" }}
+                    className={classes.learnButton}
+                  >
+                    <span style={{ marginRight: 10 }}>Learn More</span>
+                    <ButtonArrow
+                      width={10}
+                      height={10}
+                      fill="white"
+                    ></ButtonArrow>
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+            {/* Card of Contact Us */}
+            <Grid
+              item
+              sm
+              style={{
+                marginRight: matchesXS ? 0 : matchesSM ? "2em" : "5em",
+                textAlign: matchesXS ? "center" : "right",
+              }}
+            >
+              <Grid container direction="column">
+                <Typography variant="h2" style={{ color: "white" }}>
+                  Contact Us
+                </Typography>
+                <Typography variant="subtitle2">Say hello! </Typography>
+                <Grid item>
+                  <Button
+                    variant="outlined"
+                    style={{ color: "white", borderColor: "white" }}
+                    className={classes.learnButton}
+                  >
+                    <span style={{ marginRight: 10 }}>Learn More</span>
+                    <ButtonArrow
+                      width={10}
+                      height={10}
+                      fill="white"
+                    ></ButtonArrow>
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <div className={classes.infoBackground} />
         </Grid>
       </Grid>
     </Grid>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -6,7 +6,7 @@ import {
 
 const arcBlue = "#0B72B9";
 const arcOrange = "#FFBA60";
-const arcGrey = "#868686"
+const arcGrey = "#868686";
 
 export default createMuiTheme({
   palette: {
@@ -38,27 +38,32 @@ export default createMuiTheme({
     h3: {
       fontFamily: "Pacifico",
       fontSize: "2.5rem",
-      color: arcBlue
+      color: arcBlue,
     },
     h4: {
       fontFamily: "Raleway",
       fontSize: "1.75rem",
       color: arcBlue,
-      fontWeight: 700
+      fontWeight: 700,
     },
     subtitle1: {
       fontSize: "1.25rem",
       fontWeight: 300,
-      color: arcGrey
+      color: arcGrey,
+    },
+    subtitle2: {
+      color: "white",
+      fontSize: "1.25rem",
+      fontWeight: 300,
     },
     learnButton: {
-    borderColor: arcBlue,
-    color: arcBlue,
-    borderWidth: 2,
-    textTransform: "none",
-    borderRadius: 50,
-    fontFamily: "Roboto",
-    fontWeight: "bold",
+      borderColor: arcBlue,
+      color: arcBlue,
+      borderWidth: 2,
+      textTransform: "none",
+      borderRadius: 50,
+      fontFamily: "Roboto",
+      fontWeight: "bold",
     },
   },
 });
